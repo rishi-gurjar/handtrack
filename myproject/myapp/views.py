20228e668a054204e8615094714bbecec694d3be
Rishi Gurjar <rishigurjar2000@gmail.com>
2023-09-01 15:45:14 -0400

    django testing

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,20 +4,26 @@ from json import dumps
   
 def send_dictionary(request):
     # create data dictionary
+    while True:
+        n = 0
+        x = 0
+        for n in range(1, 10):
+            print(n)
+            x+=1
 
-    n = 0
-    for n in range(1, 10):
-        print(n)
+        dataDictionary = {
+            'hello': 'World',
+            'geeks': 'forgeeks',
+            'ABC': 123,
+            456: 'abc',
+            14000605: 1,
+            'list': ['zeeeeks', n, 'geeks'],
+            'dictionary': {'you': 'can', 'send': 'anything', 3: 1}
+        }
 
-    dataDictionary = {
-        'hello': 'World',
-        'geeks': 'forgeeks',
-        'ABC': 123,
-        456: 'abc',
-        14000605: 1,
-        'list': ['zeeeeks', n, 'geeks'],
-        'dictionary': {'you': 'can', 'send': 'anything', 3: 1}
-    }
-    # dump data
-    dataJSON = dumps(dataDictionary)
-    return render(request, 'landing.html', {'data': dataJSON})
+        # dump data
+        dataJSON = dumps(n)
+        return render(request, 'landing.html', {'data': dataJSON})
+    
+
+    # RUN python manage.py runserver
